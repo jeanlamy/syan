b5afe80d36a1a0cb04f7ea4f365926ea6bd886f6
Jean <jean.lamy@gmail.com>
2016-05-13 11:46:59 +0200

    add nelmio/cors-bundle to allow access to api anywhere (for test purpose) implement delete action

--- README.md
+++ README.md
@@ -19,3 +19,19 @@ Méthodes :
 * api/ideas/<id>.json : détail d'une idée
 
 
+## syan-front
+
+Front end avec AngularJS using 1.5.x
+
+Voir tutoriel ici : http://www.sitepoint.com/kickstart-your-angularjs-development-with-yeoman-grunt-and-bower/
+(attention, l'utilisation des outils décrits dans ce tutoriel nécessite Ruby sous Windows)
+Lancer l'appli avec grunt :
+  grunt serve --port 9000
+
+
+@todo :
+- formulaire d'ajout/modification/suppression
+- api de suppression d'une idée
+- api de modification d'une idée
+- api de creation d'une idée
+- finalisation
--- syan-api/src/AppBundle/Controller/IdeasController.php
+++ syan-api/src/AppBundle/Controller/IdeasController.php
@@ -1,20 +1,36 @@
 <?php
 
 namespace AppBundle\Controller;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\RestBundle\Controller\Annotations\View;
 use AppBundle\Entity\Idea;
+
 
 class IdeasController extends Controller implements ClassResourceInterface
 {
+    /**
+     * "get_ideas"     [GET] /ideas
+     * Get a list of all ideas
+     *
+     * @return array
+     */
     public function cgetAction()
     {
         $ideas = $this->getDoctrine()
             ->getRepository('AppBundle:Idea')->findAll();
         return $ideas;
-    } // "get_users"     [GET] /users
+    }
 
-
+    /**
+     * "get_idea"      [GET] /ideas/{id}
+     *
+     * Get idea information
+     *
+     * @param $id
+     * @return null|object
+     */
     public function getAction($id)
     {
         $idea = null;
@@ -26,5 +42,25 @@ class IdeasController extends Controller implements ClassResourceInterface
         }
 
         return $idea;
+    }
+
+    /**
+     * "delete_idea"          [DELETE] /ideas/{id}
+     *
+     * Delete an idea. Return a status code 204
+     *
+     * @param $id
+     *
+     * @View(statusCode=204)
+     *
+     */
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $idea = $em->getRepository('AppBundle:Idea')->find($id);
+        if($idea) {
+            $em->remove($idea);
+            $em->flush();
+        }
     }
 }
