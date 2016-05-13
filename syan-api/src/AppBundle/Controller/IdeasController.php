adba659a091504ea1e6f9d5f692776e2a141aeca
Jean <jean.lamy@gmail.com>
2016-05-13 13:44:07 +0200

    Finish CRUD operations

--- syan-api/src/AppBundle/Controller/IdeasController.php
+++ syan-api/src/AppBundle/Controller/IdeasController.php
@@ -3,9 +3,11 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Routing\ClassResourceInterface;
 use FOS\RestBundle\Controller\Annotations\View;
 use AppBundle\Entity\Idea;
+
 
 
 class IdeasController extends Controller implements ClassResourceInterface
@@ -63,4 +65,40 @@ class IdeasController extends Controller implements ClassResourceInterface
             $em->flush();
         }
     }
+
+    /**
+     * "new_ideas"            [GET] /ideas/new
+     *
+     * @View(statusCode=204)
+     */
+    public function cpostAction(Request $request)
+    {
+        $idea = new Idea();
+        $idea->setName($request->get('name'));
+        $idea->setDescription($request->get('description'));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($idea);
+        $em->flush();
+    }
+
+    /**
+     * "put_idea"             [PUT] /ideas/{id}
+     * Update an idea
+     *
+     * @param $id
+     * @param Request $request
+     * @View(statusCode=204)
+     */
+    public function postAction($id, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $idea = $em->getRepository('AppBundle:Idea')->find($id);
+        $idea->setName($request->get('name'));
+        $idea->setDescription($request->get('description'));
+        $em->flush();
+    }
+
+
+
 }
